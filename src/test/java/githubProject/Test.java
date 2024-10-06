776765af79e5ac56ca3f33e065f25bba95356503
Swaphoenix96 <Swaphoenix96@gail.com>
2024-10-06 16:21:17 +0530

    changes in login method

--- src/test/java/githubProject/Test.java
+++ src/test/java/githubProject/Test.java
@@ -2,9 +2,16 @@ package githubProject;
 
 public class Test {
 	
+	public static void login()
+	{
+		System.out.println("updated login method");
+	}
+	
 	public static void main(String[]args)
 	{
+		login();
 		System.out.println("This is Working Directory");
+		System.out.println("This is After First Push");
 		System.out.println("This is After First Push");
 		
 	}
